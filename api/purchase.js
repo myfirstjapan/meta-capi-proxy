b78282a13f733eddf0ea22ff05feac87fefec10a
myfirstjapan <ichikawa@myfirst.tech>
2025-07-03 10:48:10 +0900

    Update purchase.js

--- api/purchase.js
+++ api/purchase.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
     } = req.body;
 
     const pixelId = '1252395909231068'; // あなたのPixel ID
-    const accessToken = 'ここにアクセストークンを貼る';
+    const accessToken = 'EAAJ3UWX2W6gBO7n8TPvQrysvGIEARZB3RnTHKN72UGKgteM6lKUGAAAqd32sSE2NNVpiZCSnZC6iIaw1JEhQOUvJ1lccvVQYhJVei8p7CDP2wquTVLLjdYNyoJ7yRRCg1aRK92zrcDNxs1EidSvPK7MQ7ZBZAnAlhQ8Qivi7AwbH55aRvzwDbXRzFzUgDKzsjnwZDZD';
 
     const hashSHA256 = (input) =>
       crypto.createHash('sha256').update(input).digest('hex');
